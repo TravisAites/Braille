2b3d3fedb7b01f199cd081e6cff99294f04e1053
travisaites21@gmail.com <>
2020-02-05 01:09:34 -0500

    this is the first commit

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Travis'PC
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         String T = "\u281E";
+         String r = "\u2866";
+         String a = "\u2801";
+         String v = "\u2827";
+         
+       System.out.println(T+r+a+v);
+    }
+    
+}
